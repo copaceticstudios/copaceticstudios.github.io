cbba29102dbfad91f9f0c922ecf4b5dd97c968a8
Michael Kent <michael.kent@garmin.com>
2018-02-13 08:00:28 -0600

    Use better type for NavBar content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 
-export type NavBarItem = { content: string | React.Element<*>, url: string }
+export type NavBarItem = { content: React.Node, url: string }
 
 export default class NavBar extends React.Component<{
   leftItems: Array<NavBarItem>,
